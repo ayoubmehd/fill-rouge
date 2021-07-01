0a5fe8a63cb4d6684199dfc4ddb35b5f25505f78
ayoubmehd <safi-_-gio-raja@live.fr>
2021-07-01 16:26:44 +0100

    feat: add end culumn end_date

--- database/migrations/2021_07_01_130457_create_subscriptions_table.php
+++ database/migrations/2021_07_01_130457_create_subscriptions_table.php
@@ -16,6 +16,7 @@ class CreateSubscriptionsTable extends Migration
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
             $table->json("payment_getway");
+            $table->date("end_date");
             $table->timestamps();
         });
     }
