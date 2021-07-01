f90d2c60e4ce3e46f21a49d6a8d93c08950e92e6
ayoubmehd <safi-_-gio-raja@live.fr>
2021-07-01 16:15:07 +0100

    feat: create migration

---
+++ database/migrations/2021_07_01_125947_create_ctm_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCtmPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ctm_posts', function (Blueprint $table) {
+            $table->id();
+            $table->text("content");
+            $table->string("platform");
+            $table->integer("like_count");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ctm_posts');
+    }
+}
