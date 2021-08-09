968eeae6bccfe6c976a8fd9921aed7886f80299a
ayoubmehd <safi-_-gio-raja@live.fr>
2021-08-09 20:14:42 +0100

    style: add style to button that give access to facebook

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,7 +1,21 @@
 <template>
-    <div>
-        <a :href="facebookLoginUrl">Link your facebook account</a>
-    </div>
+    <b-container>
+        <b-row align-h="center" align-v="center">
+            <b-col cols="md-5">
+                <b-card
+                    bg-variant="dark"
+                    text-variant="white"
+                    class="h-100"
+                    body-class="d-flex justify-content-center"
+                >
+                    <b-button variant="primary" :href="facebookLoginUrl">
+                        <b-icon icon="facebook"></b-icon>
+                        Give app facebook access
+                    </b-button>
+                </b-card>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
