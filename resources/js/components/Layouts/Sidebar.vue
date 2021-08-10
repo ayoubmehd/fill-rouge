383a41e2362176f38beaea145638ec00663942dd
ayoubmehd <safi-_-gio-raja@live.fr>
2021-08-10 13:37:17 +0100

    feat: add active class

--- resources/js/components/Layouts/Sidebar.vue
+++ resources/js/components/Layouts/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
     <b-sidebar id="main-sidebar" bg-variant="dark" text-variant="light" shadow>
         <b-nav class="mt-5" vertical>
-            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item :to="{ name: 'Home' }" exact>Home</b-nav-item>
             <b-nav-item :to="{ name: 'Posts' }">Posts</b-nav-item>
             <b-nav-item :to="{ name: 'AddPost' }">Add Post</b-nav-item>
             <b-nav-item :to="{ name: 'AppManagment' }">
